b6b70acda083e2859f8b96970d09f0238f0290ce
Sahand-Admin <ghotbijr@gmail.com>
2018-05-31 23:14:33 -0700

    Updating tests

--- app/src/androidTest/java/com/example/sahand/homework3/MainActivityTest.java
+++ app/src/androidTest/java/com/example/sahand/homework3/MainActivityTest.java
@@ -70,6 +70,8 @@ public class MainActivityTest {
         onView(withId(R.id.occupation_field)).perform(typeText("Carpenter")).perform(closeSoftKeyboard());
         onView(withId(R.id.description_field)).perform(typeText("I'm a pretty cool dude")).perform(closeSoftKeyboard());
         onView(withId(R.id.submit_button)).perform(click());
+        onView(withText("CANCEL")).perform(click());
+
         onView(withId(R.id.name_and_age_display)).check(matches(withText("John Michaels, 29")));
         onView(withId(R.id.occupation_display)).check(matches(withText("Carpenter")));
         onView(withId(R.id.description_display)).check(matches(withText("I'm a pretty cool dude")));
